6419ab382faa3d095248e5488998a14227f1123f
Chenhao Wang <implexrr@gmail.com>
2023-04-09 17:01:21 -0400

    Add generateLocation.js

--- src/generateLocation.js
+++ src/generateLocation.js
@@ -1,6 +1,6 @@
 export default function generatLocation() {
   const location = document.createElement('div');
-  home.textContent = '1280 Main Street West, Hamilton, ON';
-  
-  return navOptions
+  location.textContent = '1280 Main Street West, Hamilton, ON';
+
+  return location;
 }
