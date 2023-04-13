66f127053817940162716b665f34a033b70eb919
Chenhao Wang <implexrr@gmail.com>
2023-04-13 19:10:00 -0400

    Fix location id bug

--- src/footer/generateLocation.js
+++ src/footer/generateLocation.js
@@ -2,6 +2,6 @@ export default function generatLocation() {
   // Generate restaurant location 
   const location = document.createElement('div');
   location.textContent = '1280 Main Street West, Hamilton, ON';
-
+  location.setAttribute('id', 'location');
   return location;
 }
