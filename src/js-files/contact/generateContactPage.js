a1996a874e3b962ffbfe3f17a0c4cc7e4e841899
Chenhao Wang <implexrr@gmail.com>
2023-05-11 15:46:10 -0400

    Create components for contact page main content

--- src/js-files/contact/generateContactPage.js
+++ src/js-files/contact/generateContactPage.js
@@ -1,11 +1,13 @@
 import '../../css-files/webpage-styles/contact.css';
 import header from '../tools/header/header.js';
+// import footer from '../tools/footer/footer';
 import underlineNavContact from '../tools/header/navbar/underlineNavContact';
 
 export default function generateContactPage() {
   const contactPage = [];
 
   contactPage.push(header());
+  // contactPage.push(footer());
 
   for (let i = 0; i < contactPage.length; i++) {
     document.querySelector('#content').appendChild(contactPage[i]);
