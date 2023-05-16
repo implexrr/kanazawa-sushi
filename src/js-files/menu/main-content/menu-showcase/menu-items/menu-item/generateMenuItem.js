44578cc5431210de90a1b3979a1f13e4ae982bcb
Chenhao Wang <implexrr@gmail.com>
2023-05-16 14:00:43 -0400

    Finish menu folder files

--- src/js-files/menu/main-content/menu-showcase/menu-items/menu-item/generateMenuItem.js
+++ src/js-files/menu/main-content/menu-showcase/menu-items/menu-item/generateMenuItem.js
@@ -1,9 +1,32 @@
-export default function generateItem(itemId) {
-  // export default function generateItem(itemId, englishName, japaneseName, amount, price)
+export default function generateMenuItem(itemId, englishName, japaneseName, _amount, _price) {
   // const salmonNigiri = generateMenuItem("salmon-nigiri", "Salmon Nigiri", "サーモン", "2pc", "6.99");
   const menuItem = document.createElement('div');
   menuItem.setAttribute('class', 'menu-item');
   menuItem.setAttribute('id', itemId);
+
+  const itemIcon = document.createElement('div');
+  itemIcon.setAttribute('class', 'item-icon');
+  menuItem.appendChild(itemIcon);
+  
+  const descriptionEnglish = document.createElement('div');
+  descriptionEnglish.setAttribute('class', 'description-english');
+  descriptionEnglish.textContent = englishName;
+  menuItem.appendChild(descriptionEnglish);
+
+  const descriptionJapanese = document.createElement('div');
+  descriptionJapanese.setAttribute('class', 'description-japanese');
+  descriptionJapanese.textContent = japaneseName;
+  menuItem.appendChild(descriptionJapanese);
+
+  const amount = document.createElement('div');
+  amount.setAttribute('class', 'amount');
+  amount.textContent = _amount;
+  menuItem.appendChild(amount);
+
+  const price = document.createElement('div');
+  price.setAttribute('class', 'price');
+  price.textContent = _price;
+  menuItem.appendChild(price);
   
 
   return menuItem;
--- src/js-files/menu/main-content/menu-showcase/menu-items/menuItems.js
+++ src/js-files/menu/main-content/menu-showcase/menu-items/menuItems.js
@@ -1,10 +1,32 @@
 import generateMenuItem from './menu-item/generateMenuItem.js';
 export default function menuItems() {
   const menuItems = []
-  // const salmonNigiri = generateMenuItem("salmon-nigiri", "Salmon Nigiri", "サーモン", "2pc", "6.99");
-  const salmonNigiri = generateMenuItem("salmon-nigiri");
+  const salmonNigiri = generateMenuItem("salmon-nigiri", "Salmon Nigiri", "サーモン", "2pc", "6.99");
+  const tunaNigiri = generateMenuItem("tuna-nigiri", "Tuna Nigiri", "まぐろ", "2pc", "6.99");
+  const shrimpNigiri = generateMenuItem("shrimp-nigiri", "Shrimp Nigiri", "エビ", "2pc", "6.99");
+  const eggNigiri = generateMenuItem("egg-nigiri", "Egg Nigiri", "卵", "2pc", "6.99");
+  const crabNigiri = generateMenuItem("crab-nigiri", "Crab Nigiri", "カニ", "2pc", "6.99");
+  const salmonAvocadoMaki = generateMenuItem("salmon-avocado-maki", "Salmon Avocado Nigiri", "サーモンアボカド", "6pc", "10.99");
+  const salmonRoeMaki = generateMenuItem("salmon-roe-maki", "Salmon Nigiri", "いくら", "6pc", "10.99");
+  const cucumberMaki = generateMenuItem("cucumber-maki", "Cucumber Maki", "キュウリ", "6pc", "7.99");
+  const crabHandRoll = generateMenuItem("crab-hand-roll", "Crab Hand Roll", "カニの手巻き", "1pc", "6.99");
+  const salmonHandRoll = generateMenuItem("salmon-hand-roll", "Salmon Hand Roll", "サーモンの手巻き", "1pc", "6.99");
+  const doubleSalmon = generateMenuItem("double-salmon", "Double Salmon", "ダブルサーモン", "6pc", "11.99");
+  const salmonDreams = generateMenuItem("salmon-dreams", "Salmon Nigiri", "サーモンの夢", "6pc", "11.99");
 
   menuItems.push(salmonNigiri);
+  menuItems.push(tunaNigiri);
+  menuItems.push(shrimpNigiri);
+  menuItems.push(eggNigiri);
+  menuItems.push(crabNigiri);
+  menuItems.push(salmonAvocadoMaki);
+  menuItems.push(salmonRoeMaki);
+  menuItems.push(cucumberMaki);
+  menuItems.push(crabHandRoll);
+  menuItems.push(salmonHandRoll);
+  menuItems.push(doubleSalmon);
+  menuItems.push(salmonDreams);
+
   const menuItemsDOM = document.createElement('div');
   menuItemsDOM.setAttribute('id', 'menu-items');
   for (let i = 0; i < menuItems.length; i++) {
